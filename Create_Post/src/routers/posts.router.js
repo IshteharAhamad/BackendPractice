e904f21c4fe67cf633fbac4655f9ce54160ae90d
IshteharAhamad <isthicarkhan@gmail.com>
2024-10-10 11:25:13 +0530

    next changes

--- Create_Post/src/routers/posts.router.js
+++ Create_Post/src/routers/posts.router.js
@@ -6,7 +6,7 @@ router.route("/create-post").post(verifyToken,createPost);
 router.route('/update-post/:id').put(verifyToken, update_Post);
 router.route('/delete-post/:id').delete(verifyToken, deletePost);
 router.route('/user-post').get(verifyToken,getMyAllPost)
-router.route('/all-posts').get(getAllPosts)
+router.route('/').get(getAllPosts)
 
 
 export default router;
